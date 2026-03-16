c35528bb29403a4ebe5f0718c55ce7a1cbc21ceb
JuanTrujilloM <juantru2211@gmail.com>
2026-03-15 21:53:55 -0500

    updated the author for the files

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Author:Juan Esteban Trujillo Montes
+ * Description: Controller responsible for managing Products
+ */
+
 namespace App\Http\Controllers;
 
 use App\Models\Product;
