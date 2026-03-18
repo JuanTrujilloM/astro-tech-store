0cf56dbe2a80d6e361310dd013a51b6343717da8
JuanTrujilloM <juantru2211@gmail.com>
2026-03-18 15:12:36 -0500

    feat: Base secure validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Review;
-use App\Policies\ReviewPolicy;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::policy(Review::class, ReviewPolicy::class);
+        //
     }
 }
