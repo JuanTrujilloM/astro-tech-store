2dae0c8bbc53654536079cacfa61ba9d5e60fa53
JuanTrujilloM <juantru2211@gmail.com>
2026-03-22 00:29:23 -0500

    feat: add factories for Product, Review, Order, and Item models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,12 +8,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
+    use HasFactory;
+
     /**
      * ORDER ATTRIBUTES
      * $this->attributes['id'] - int - contains the order primary key (id)
