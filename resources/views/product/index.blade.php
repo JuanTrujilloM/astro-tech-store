c0171e42466a2cd4479cf72f34791f46261eb075
JuanTrujilloM <juantru2211@gmail.com>
2026-03-23 13:25:19 -0500

    fix: Visual updates

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -7,9 +7,9 @@
 @section('content')
 
   @if (session('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <div class="alert alert-success alert-dismissible fade show mb-4" role="alert">
       {{ session('success') }}
-      <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
   @endif
 
